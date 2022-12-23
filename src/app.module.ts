145bf4e7376d199be7b9985c70838c54e1b4c49e
itsaln <itsaln23@gmail.com>
2022-12-23 17:02:21 +0500

    init

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+import { AppController } from '@app/app.controller'
+import { AppService } from '@app/app.service'
+
+@Module({
+	controllers: [AppController],
+	providers: [AppService]
+})
+export class AppModule {}
